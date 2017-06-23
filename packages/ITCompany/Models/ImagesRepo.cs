682c837cae2f9b55af31c915cc244a4ca736eac5
Nezernosh <nereznosh@gmail.com>
2017-06-23 17:27:53 +0400

    Almost done
    Fixed edit, added pictures.

---
+++ packages/ITCompany/Models/ImagesRepo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ITCompany.Models
+{
+    public class ImagesRepo
+    {
+        private static string imageSource;
+
+        public byte[] ClientsGet()
+        {
+            imageSource = ConfigurationManager.AppSettings["ClientsImageSource"];
+            return File.ReadAllBytes(imageSource);
+        }
+
+        public byte[] EmployeesGet()
+        {
+            imageSource = ConfigurationManager.AppSettings["EmployeesImageSource"];
+            return File.ReadAllBytes(imageSource);
+        }
+
+        public byte[] ProjectsGet()
+        {
+            imageSource = ConfigurationManager.AppSettings["ProjectsImageSource"];
+            return File.ReadAllBytes(imageSource);
+        }
+
+        public void Update(byte[] content)
+        {
+            File.WriteAllBytes(imageSource, content);
+        }
+    }
+}
---
+++ packages/ITCompany/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ITCompany.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        [Required]
+        public int ClientID { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public decimal Budget { get; set; }
+    }
+}
